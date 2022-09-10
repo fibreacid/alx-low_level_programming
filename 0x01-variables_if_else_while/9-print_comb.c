58ff73a70837b62e5e31c7674050b0581ba5a61e
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-09 17:47:13 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,17 +12,19 @@
 int main(void)
 
 {
+
 int c;
 
-for (c = 48; c <= 57; c++)
+for (c = 0; c <= 9; c++)
+
 {
-putchar(c);
-if (c != 57)
-{
+	putchar((c % 10) + '0');
+	if (c == 9);
+	continue;
+	
 	putchar(',');
 	putchar(' ');
 }
-}
 putchar('\n');
 return (0);
-} 
+}	
