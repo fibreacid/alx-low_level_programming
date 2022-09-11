86d8638c3f6914834c4f84ddf2ba6eaf9db1b3e0
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-11 06:20:22 -0700

    prints numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+/**
+ * main - Prints the numbers from 00 to 99,numbers seperated
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit2 < 10; digit1++)
+	{
+		putchar((digit1 % 10) + '0');
+		putchar((digit2 % 10) + '0');
+
+		if (digit1 == 9 && digit2 == 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
