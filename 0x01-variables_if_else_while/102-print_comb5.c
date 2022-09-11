e0a58a9740dbd6389ba09fa6bd3bd1451d7f4b61
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-11 07:23:30 -0700

    combinatin of two two-numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 99; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = 0; digit2 < 99; digit2++)
+		for (digit2 = 0; digit2 < 9; digit2++)
 		{
 			putchar(digit1);
 			putchar(digit2);
 
-			if (digit1 != 99 && digit2 != 99)
+			if (digit1 != 9 && digit2 != 9)
 			{
 				putchar(',');
 				putchar(' ');
