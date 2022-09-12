bd6edf0034777429e2ae1b69d0edcf08f09672b7
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-12 12:49:49 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,13 +16,14 @@ int main(void)
 	{
 		for (b = a + 1; b < 10; b++)
 		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
 			if (a == 8 && b == 9)
 				continue;
-			{
-				putchar((a % 10) + '0');
-				putchar((b % 10) + '0');
-				putchar(',');
-				putchar(' ');
+
+			putchar(',');
+			putchar(' ');
 			}
 		}
 	}
