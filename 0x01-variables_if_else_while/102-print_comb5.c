8fa02ddb1bce9bacaeb1e1a769efb9bd03b6b1eb
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-12 01:52:47 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,14 +14,13 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar((digit1 / 10) + '0');
-			putchar((digit1 % 10) + '0');
+			putchar(digit1 / 10);
+			putchar(digit1 % 10);
 			putchar(' ');
-			putchar((digit2 / 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar(digit2 / 10);
+			putchar(digit2 % 10);
 
-			if (digit1 == 9 && digit2 == 9)
-				continue;
+			if (digit1 != 9 && digit2 != 9)
 			{
 				putchar(',');
 				putchar(' ');
