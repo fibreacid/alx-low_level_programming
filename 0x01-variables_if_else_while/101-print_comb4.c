921aeb245eaf89574d2b756dec6d45b049a84d60
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-12 11:41:04 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - entry point
  *
  * decription - program that prints all possible different combinations of three digits.
@@ -14,17 +14,19 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '1'; b <= '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '2'; c <= '9'; c++)
+			for (c = '0'; c <= '9'; c++)
 			{
-				if (!(a == b && b == c))
+				if (c > b && b > c)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					putchar(',');
-					putchar(' ');
+					if (a != 7 || c != 8)
+					{
+						putchar(',');
+						putchar(' ');
 				}
 			}
 		}
