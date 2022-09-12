4f1b331bf9421f0beb2034089e836d3d099da050
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-12 14:11:44 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int digit1, digit2, digit3, digit4;
 
-	for (digit1 = 48; digit1 < 58; digit1++)
+	for (digit1 = 48; digit1 < 57; digit1++)
 	{
-		for (digit2 = 48; digit2 < 58; digit2++)
+		for (digit2 = 48; digit2 < 57; digit2++)
 		{
-			for (digit3 = digit1 + 1; digit3 < 60; digit3++)
+			for (digit3 = digit1 + 1; digit3 < 57; digit3++)
 			{
-				for (digit4 = 48; digit4 < 58; digit4++)
+				for (digit4 = 48; digit4 < 57; digit4++)
 				{
 					/*if (digit1 != '9' && digit2 != '9' && digit3 != '9' && digit4 != '9')*/
 					if (!(digit1 == digit2 && digit3 == digit4))
@@ -26,6 +26,7 @@ int main(void)
 						putchar(' ');
 						putchar(digit3);
 						putchar(digit4);
+
 						putchar(',');
 					}
 				}
