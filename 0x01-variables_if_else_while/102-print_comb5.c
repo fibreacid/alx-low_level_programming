8ce0f4050ad07fdd3f81d49802f85ad69b90b3f7
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-12 10:43:02 -0700

    combination of two two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int digit1, digit2, digit3, digit4;
 
-	for (digit1 = '0'; digit1 < '10'; digit1++)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		for (digit2 = '0'; digit2 < '10'; digit2++)
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
-			for (digit3 = digit1; digit3 < '10'; digit3++)
+			for (digit3 = digit1; digit3 < 10; digit3++)
 			{
-				for (digit4 = '0'; digit4 < '10'; digit4++)
+				for (digit4 = 0; digit4 < 10; digit4++)
 				{
 					/*if (digit1 != '9' && digit2 != '9' && digit3 != '9' && digit4 != '9')*/
 					if (!(digit1 == digit2 && digit3 == digit4))
