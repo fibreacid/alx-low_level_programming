3c025b993f72bdacc704684408353bc97f0cb651
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-13 14:44:03 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabets in lowercase
+ *
+ * return 0;
+ *
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description - prints 10 timesthe alphabet in lowercase
+ *
+ * Return 0;
+ */
+
+void print_alphabet_x10(void);
+{
+	int count = 0;
+	char start = 'a';
+	char end = 'z';
+
+	char letter = start;
+
+	while(count++ <= 9)
+	{
+		for (letter <= end)
+			_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
