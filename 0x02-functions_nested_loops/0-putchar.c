f392a63fe5ef10fc964dadc19bf9631746179334
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-13 07:24:00 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,28 @@
  * return 0;
  */
 
-int main()
+int main(void)
 {
-	_putchar('_');
+	int a0 = '_';
+	int a1 = 'p';
+	int a2 = 'u';
+	int a3 = 't';
+	int a4 = 'c';
+	int a5 = 'h';
+	int a6 = 'a';
+	int a7 = 'r';
+	{
+		_putchar(a0);
+		_putchar(a1);
+		_putchar(a2);
+		_putchar(a3);
+		_putchar(a4);
+		_putchar(a5);
+		_putchar(a6);
+		_putchar(a7);
+		_putchar('\n');
+	}
+	/*_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,5 +35,6 @@ int main()
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	return (0);*/
+return (0);
 }
