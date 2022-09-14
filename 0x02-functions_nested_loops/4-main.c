19b6983f302aaadf86b6f67ce60025c3be5b85b5
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-13 21:02:13 -0700

    task

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -11,7 +11,7 @@ int main(void)
 	r = _isalpha('o');
        	_putchar(r + '0');
 	r = _isalpha(108);
-	_putchar(r + '0')
+	_putchar(r + '0');
 	r = _isalpha(';');
        	_putchar(r + '0');
        	_putchar('\n');
