196a9ec20db77767c2edad00531c76eb0c0d75a6
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-15 14:11:09 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,7 @@
 
 /**
  * _isupper - Checks for uppercase character.
+ * @c: The character to be checked.
  * Returns: 1 if c is uppercase, 0 otherwise.
  */
 
@@ -11,7 +12,7 @@ int _isupper(int c)
 {
 	char c;
 
-	if (c == 'C')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
