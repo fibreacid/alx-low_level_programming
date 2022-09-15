dcfa47797a9454f76943887584487d3b6cb8b53d
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-15 14:36:13 -0700

    0-isupper tash 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * 
+ * Description: Header file containing prototypes for all function
+ * used in the 0x03-more_functions_nested_loops directory.
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 
