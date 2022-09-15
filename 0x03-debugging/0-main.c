950707a438a028912644e276475c9827bee1963c
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-15 08:58:07 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "include"
 
 /**
- * positive_or_negative - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer,
+ *        is positive or negative
  * Return: 0
  */
 int main(void)
