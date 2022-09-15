39629c4a416d1763921a556580d4428ffa621b02
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-15 03:39:58 -0700

    test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - Tests function that prints if integer,
+ *                        is positive or negative.
+ *
+ * Return: Positive or Negative
+ */
+int positive_or_negative(void)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%i is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%i is negative", i);
+	}
+	return (0);
+}
+
