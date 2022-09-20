89fb8652d782756697ac7f95b9cbe4f05ccc0854
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-20 12:06:18 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,13 @@
  * Return: A pointer to the destination string @dest.
  */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
-
-	while (src[index])
+	int i = 0;
+	
+	for (; src[i] != '\0'; i++)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
