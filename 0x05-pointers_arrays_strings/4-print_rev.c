7f087d1198dceed3719e8c7cce6be31c6e96c4e0
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-19 20:10:18 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+	int index;
+	int length = 0;
+
+
+	while (s[index++])
+	{
+		++length;
+	}
+	--length;
+
+	for (index = 0; *s != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
