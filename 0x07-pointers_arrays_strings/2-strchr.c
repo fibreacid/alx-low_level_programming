de020b87bc805c03b25388545c13e1a06f5a8ccd
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-26 07:20:44 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * _strchr - locates a character in a string.
  * @c: input
  * @s: input
- * @null: input
+ * return: c
  */
 
 char *_strchr(char *s, char c)
@@ -13,6 +13,6 @@ char *_strchr(char *s, char c)
 	unsigned char i;
 
 	for (i = 0; i < c; i++)
-		s[i] = i != '\0';
+		s[i] = '\0';
 	return (s);
 }
