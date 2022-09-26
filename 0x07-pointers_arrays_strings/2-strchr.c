8ae8c77d242ffc1afd2de71e055afd3c0c20d336
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-26 07:17:51 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locates a character in a string.
+ * @c: input
+ * @s: input
+ * @null: input
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned char i;
+
+	for (i = 0; i < c; i++)
+		s[i] = i != '\0';
+	return (s);
+}
