a1efde3ffc217658aa9c530343a8273bae0bc2d2
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-26 07:04:38 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/*
+ * _memcpy - copies memory area.
+ * @dest: destination
+ * @src: n bytes to be copied.
+ * Return: dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
