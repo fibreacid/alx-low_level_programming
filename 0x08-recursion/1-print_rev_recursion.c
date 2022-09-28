096bfb1a8f615a96b7a8283f518d8d71c063a9b2
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-28 09:48:31 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,15 +7,12 @@
  * Return: always 0.
  */
 
-void _print_rev_recursion(char *s)
+void _print_rev_recursion(c)
 {
-	if (*s != '\0')
+
+	if (*s)
 	{
-		_putchar(*s);
 		_print_rev_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
+		_putchar(*s);
 	}
 }
