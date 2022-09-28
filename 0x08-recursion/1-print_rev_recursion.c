980e3cc78768d4e11c9c1ba9a05945d871dba9cf
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-28 08:21:49 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: input.
+ * Return: always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
