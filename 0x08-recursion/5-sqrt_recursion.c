5ad31d9864d276701597b6ddad464e5d905c1b06
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-29 10:03:01 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,12 +10,12 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n > 1)
-	{
-		return (n + _sqrt_recursion(n - 1));
-	}
-	else (n < 1)
+	if (n < 1)
 	{
 		return (-1);
+	}
+	else if (n > 1)
+	{
+		return (n + _sqrt_recursion(n - 1));
 	}
 }
