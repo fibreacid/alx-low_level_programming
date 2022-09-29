ce90eff5dbc0914ad41d5099bbf6c214af2d4acd
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-29 07:53:16 -0700

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,9 +15,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y == 0)
+	{
+		return (1);
+	}
 	else 
 	{
-		base *= _pow_recursion(y - 1);
+		base *= _pow_recursion(x, y - 1);
 		return (base);
 	}
 }
