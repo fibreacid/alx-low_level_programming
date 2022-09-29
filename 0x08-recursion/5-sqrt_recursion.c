c3d9a1b3012e69cf9b4500f2800cc2a66e9c9637
Thabo Ramodike <thabzinrmk@gmail.com>
2022-09-29 09:51:59 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,18 +4,18 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: input
- * Return: negative one if n does not have a natural 
+ * Return: negative one if n does not have a natural
  * square root
  */
 
 int _sqrt_recursion(int n)
 {
-	if ( n > 0)
+	if (n > 0)
 	{
 		return (n + _sqrt_recursion(n - 1));
 	}
 	else
 	{
-		return (-1);
+		return (1);
 	}
 }
