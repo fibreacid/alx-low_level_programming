55d978d2184499d6330515a4bd7d3ef258308d7a
Thabo Ramodike <thabzinrmk@gmail.com>
2022-10-04 08:40:43 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,19 +17,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	{
 		return (0);
-	}
+
 	ptr  = malloc(sizeof(char) * size);
 
 	if (ptr == 0)
-	{
 		return (0);
-	}
 
 	for (i = 0; i < size; i++)
-	{
 		ptr[i] = c;
-	}
+	
 	return (ptr);
 }
