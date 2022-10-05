e4f88839939363f1c46ff3cd50a25f2ad1561776
Thabo Ramodike <thabzinrmk@gmail.com>
2022-10-05 09:48:54 -0700

    tast1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.c>
+
+/**
+ * _strdup -  returns a pointer to a new string which 
+ * is a duplicate of the string str.
+ * @str: origin of string.
+ * Return: NULL if str = NULL.
+ */
+
+char *_strdup(char *str)
+{
+	char duplicate = strlen(str);
+
+	str = malloc(sizeof(char) * duplicate);
+
+	if (str == NULL)
+		return (NULL);
+
+	while (duplicate < str)
+		str[duplicate] = '\0';
+
+	return (str);
+	free (str);
+}
