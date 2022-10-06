82cc144da76e2e5512a0b90e7a8bd2be71cac26e
Thabo Ramodike <thabzinrmk@gmail.com>
2022-10-06 07:46:11 -0700

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,15 +7,14 @@
 
 void *malloc_checked(unsigned int b)
 {
-	unsigned int n;
 	int *pts;
 
-	pts = (int*)malloc(n * sizeof(int));
+	pts = (int*)malloc(b * sizeof(int));
 
 	if (pts == NULL)
 		return (98);
 
-	for (int i = 0; i < n; ++n)
+	for (int i = 0; i < b; ++n)
 		pts[i] = b;
 
 	return (pts);
