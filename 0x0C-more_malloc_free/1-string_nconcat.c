cea7d934e08acf668766478e8439c66ce15f9d67
Thabo Ramodike <thabzinrmk@gmail.com>
2022-10-06 08:38:40 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int *mem;
-	int len, j;
+	unsigned int len = n, index;
 
-	mem = malloc(n);
+	if (s1 == NULL)
+		s1 = "";
 
-	while (s1[len] != '\0')
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[index] != '\0')
 	{
 		++len;
 	}
 
-	for (j = 0; s2[j] != '\0'; ++j; ++len)
-	{
-		s1[len] = s2[j];
-	}
-	s1[len] = '\0';
+	mem = malloc(sizeof(char) * (len + 1));
 
 	if (mem == NULL)
 		return (NULL);
 
-	for (i = 0; i < n; i++)
-		len++;
+
+	for (index = 0; s1[index] != '\0'; ++index)
+	{
+		mem[len++] = s1[index];
+	}
+
+	for (index = 0; s2[index] && index < n; index++)
+		mem[len++] = s2[index];
+
+	mem[len] = '\0';
 
 	return (mem);
 }
