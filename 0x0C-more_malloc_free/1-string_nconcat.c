17e32639fc12f253101c25c95074f10e9315a713
Thabo Ramodike <thabzinrmk@gmail.com>
2022-10-07 13:47:34 -0700

    tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
-#include <string.h>
-
-/*
- * string_nconcat - concatenates two strings.
- * @s1: input
- * @s2: input
- * return: string
+/**
+ * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
+ * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
+ * @s1: input one
+ * @s2: input two
+ * @n: s2's number of bytes
+ * Return: 0
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int *stringAdd;
+	char *arr;
+	unsigned int i, j, co, co_2;
 
 	if (s1 == NULL)
-		s1[] = " ";
-
+		s1 = "";
 	if (s2 == NULL)
-		s2[] = " ";
+		s2 = "";
 
-	stringAdd = malloc(sizeof(int) * n);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
 
-	if (stringAdd == NULL)
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+
+	if (n < j)
+		j = n;
+
+	j += i;
+	arr = malloc(sizeof(char *) * (j + 1));
+
+	if (arr == NULL)
 		return (NULL);
 
-	int i;
-
-	for (i = 0; i != '\0'; i++)
-		s1[i] = strlen(s1);
-
-	s1[i] = '\0';
-
-	int length;
-
-	for (length = 0; n >= s2[length]; length++)
-		s2[length] = n;
-
-	stringAdd = strcat(s1 , s2)
-		return (stringAdd);
+	for (co = 0; co < i; co++)
+		arr[co] = s1[co];
+	for (co_2 = 0; co < j; co_2++)
+	{
+		arr[co] = s2[co_2];
+		co++;
+	}
+	co++;
+	arr[co] = '\0';
+	return (arr);
 }
