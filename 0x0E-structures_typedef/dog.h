cd18a96556d5d4532d195b0843234bc32be83505
Thabo Ramodike <thabzinrmk@gmail.com>
2022-10-18 05:29:57 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * struct dog - structure
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
-
 struct dog
 {
 	char *name;
@@ -17,6 +14,14 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-#endif /* DOG_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
